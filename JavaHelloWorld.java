12c8edd9a610f877ffd422091a4cd7a481ecd0dd
Irvin Gray <ibahtin@gmail.com>
2015-08-10 22:28:53 +0300

    Add third println method and change first string.

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,6 +1,7 @@
 public class JavaHelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("First string.");
     System.out.println("Second string.");
+    System.out.println("Third string.");
   }
 }
